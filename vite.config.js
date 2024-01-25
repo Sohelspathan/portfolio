22c38a7993972d4a12df1a437f6a78a6ee68169c
Sohel Pathan <76274822+Sohelspathan@users.noreply.github.com>
2024-01-25 21:23:47 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   build: {
+  build: {
     rollupOptions: {
       external: ['@mui/icons-material/Github'],
     },
+  },
   plugins: [react()],
-  base: "/vite-deploy/"
+  base: "/vite-deploy/",
 })
